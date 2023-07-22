6fdd6fc9a7a58c770f5f1441519b9d0e5dd70523
Mitr Joshi <114912151+Mitrjoshi@users.noreply.github.com>
2023-07-22 18:19:16 +0530

    Delete src/src directory

--- src/src/layouts/index.js
+++
@@ -1,6 +0,0 @@
-import Navbar from "./Navbar.jsx";
-import Footer from "./Footer.jsx";
-import Content from "./Content.jsx";
-import Sidemenu from "./Sidemenu.jsx";
-
-export { Navbar, Footer, Content, Sidemenu };
